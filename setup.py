eb18b9a93cb4d6a691d530e5c0c2e4b75db26e54
Yasakov Artem <jasakovartem@gmail.com>
2013-04-08 15:55:05 +0400

    Updating for pep8

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from distutils.core import setup
 
 
-def main():
-    setup(name='ExecuteCommand',
+setup(name='ExecuteCommand',
       version='1.0',
       py_modules=['executecommand'])
 
-    setup(name='TestExecuteCommand',
-          version='1.0',
-          py_modules=['testexecutecommand'])
-
-if __name__ == "__main__":
-    main()
+setup(name='TestExecuteCommand',
+      version='1.0',
+      py_modules=['testexecutecommand'])
