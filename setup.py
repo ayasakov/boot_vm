fff3aef7a33c55ff7591957e86c3a253da7768d1
Yasakov Artem <jasakovartem@gmail.com>
2013-04-09 12:37:46 +0400

    Modified installation file.

--- setup.py
+++ setup.py
@@ -3,8 +3,4 @@ from distutils.core import setup
 
 setup(name='ExecuteCommand',
       version='1.0',
-      py_modules=['executecommand'])
-
-setup(name='TestExecuteCommand',
-      version='1.0',
-      py_modules=['testexecutecommand'])
+      py_modules=['executecommand', 'testexecutecommand'])
